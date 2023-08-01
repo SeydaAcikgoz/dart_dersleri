ec6e46268b42c070af16088f35d3f717edbb1a2a
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-01 17:24:04 +0300

    Add files via upload

---
+++ switch_kullanimi.dart
@@ -0,0 +1,41 @@
+/**
+ * switch: ard arda if -else if kullanmak yerine genellikle switch yapısı tercih edilir
+ * her bir case'den sonra break denilerek switch yapısından cıkılmalıdır.
+ * 
+ *switch kullanırken sadece int ve string veri türleri kullanılır, boolean veya double kullanılmaz.
+*/
+main(List<String> args) {
+  String notDegeri = 'SDFSDF';
+
+  switch (notDegeri) {
+    case "AA":
+      print("Notunuz 90 - 100 aralığındadır");
+      break;
+
+    case "BA":
+      print("Notunuz 80 - 90 aralığındadır");
+      break;
+
+    case "BB":
+      print("Notunuz 70 - 80 aralığındadır");
+      break;
+
+    case "CB":
+      print("Notunuz 60 - 70 aralığındadır");
+      break;
+
+    case "CC":
+      print("Notunuz 50 - 60 aralığındadır");
+      break;
+
+    case "FF":
+      print("Notunuz 50den düşük, çok çalışmanız gerekiyor.");
+      break;
+
+    default:
+      {
+        print("Hatalı değer girildi");
+      }
+  }
+
+}
