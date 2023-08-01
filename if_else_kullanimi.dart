5eea99b39155bcfc8b7ccdb3e1d07446d58fb45c
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-01 16:58:44 +0300

    added note range example

--- if_else_kullanimi.dart
+++ if_else_kullanimi.dart
@@ -18,4 +18,23 @@ main(List<String> args) {
   } else {
     print("Verdiğiniz sayılar birbirne eşittir");
   }
+    print("*************************************************");
+
+  int notDegeri = 150;
+
+  if (notDegeri >= 90 && notDegeri <= 100) {
+    print("Notunuz : AA");
+  } else if (notDegeri >= 80 && notDegeri < 90) {
+    print("Notunuz : BA");
+  } else if (notDegeri >= 70 && notDegeri < 80) {
+    print("Notunuz : BB");
+  } else if (notDegeri >= 60 && notDegeri < 70) {
+    print("Notunuz : CB");
+  } else if (notDegeri >= 50 && notDegeri < 60) {
+    print("Notunuz : CC");
+  } else if (notDegeri >= 0 && notDegeri < 50) {
+    print("Notunuz çok düşük yazmaya tenezzül bile etmedik");
+  } else {
+    print("Hatalı veya eksik giriş");
+  }
 }
