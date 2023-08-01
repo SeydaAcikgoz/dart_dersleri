ccc6556b9b96c75b9879aba969fa0823c5e5f4f4
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-01 16:58:03 +0300

    added if-elseif-else

--- if_else_kullanimi.dart
+++ if_else_kullanimi.dart
@@ -9,4 +9,13 @@ main(List<String> args) {
     print("$sayi1 sayısı $sayi2 sayısından küçüktür");
   }
 
+  print("**************************************************");
+
+  if (sayi1 < sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısından küçüktür");
+  } else if (sayi2 < sayi1) {
+    print("$sayi2 sayısı $sayi1 sayısından küçüktür");
+  } else {
+    print("Verdiğiniz sayılar birbirne eşittir");
+  }
 }
