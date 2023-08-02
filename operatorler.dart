67b5158dc924521a75917411243f0df089250263
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-02 22:23:11 +0300

    Update operatorler.dart

--- operatorler.dart
+++ operatorler.dart
@@ -65,4 +65,15 @@ print(!kosul1);
   print(sayi2++);
   print(++sayi2);
 
+   //ISLEM ONCELIGI
+
+  int s1 = 10, s2 = 5;
+  double sonuc = 0;
+
+  sonuc = ((s1 + s2 * 3 - s2) + s2 - s1 * 5 + s1).toDouble();
+  print(sonuc);
+
+  sonuc = (s1 * s2 + 4 / 2) + (s1++ * s2) + (++s1);
+  print(sonuc);
+
 }
