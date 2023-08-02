6ea92de0a4dca9f26378e07a19a082a5c65bbe51
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-02 22:22:29 +0300

    Update operatorler.dart

--- operatorler.dart
+++ operatorler.dart
@@ -51,4 +51,18 @@ print(kosul1 && kosul2);
 print(kosul1 || kosul2);
 print(!kosul1);
 
+   //ARTTIRMA AZALTMA OPERATORLERI
+  int sayi1 = 10;
+  sayi1 = sayi1 + 1;
+  sayi1 += 5; // sayi1 = sayi1 + 5
+  print(sayi1);
+
+  sayi1++;
+  print(sayi1);
+
+  int sayi2 = 10;
+
+  print(sayi2++);
+  print(++sayi2);
+
 }
