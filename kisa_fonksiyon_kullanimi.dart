31031d557dcc161adf579a4bb978ec510f4b82b3
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-12 22:15:23 +0300

    Update kisa_fonksiyon_kullanimi.dart

--- kisa_fonksiyon_kullanimi.dart
+++ kisa_fonksiyon_kullanimi.dart
@@ -9,6 +9,12 @@ void main(List<String> args) {
 
   print("fark: $fark");
 
+  print("çarpım : "+ sayilariCarp(12,6).toString());
+
+  print("Büyük olan sayı : "+maxOlaniBul(14,9).toString());
+  
+  print("Küçük olan sayı : "+minOlaniBul(14,9).toString());
+
 }
 
 void sayilariTopla(){
@@ -19,4 +25,19 @@ void sayilariTopla(){
 int sayilariCikar(int s1, int s2){
   return (s1 - s2) ;
 }
+
+int sayilariCarp(int s1, int s2) => s1 * s2; 
+/*
+int maxOlaniBul(int s1, int s2){
+
+  if(s1 < s2){
+    return s2;
+  }else {
+    return s1;
+  }
+}*/
+
+int maxOlaniBul(int s1, int s2) => (s1 < s2) ? s2 : s1;
+
+minOlaniBul(int s1, int s2) => (s1 < s2) ? s1 : s2;
 
