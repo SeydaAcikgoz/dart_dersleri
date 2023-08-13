bc1002f90eb74a66aed5a85f5a9e0ff3264f0573
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-13 23:10:12 +0300

    Add files via upload

---
+++ sabit_boyutlu_listeler.dart
@@ -0,0 +1,21 @@
+/**
+ * List ikiye ayrılır
+ *  1-Sabit uzunluklu
+ *  2-Büyüyen listeler
+ * 
+ * Diğer dillerde dizi olarak bilinen yapılar dart dilinde sabit uzunluklu liste olarak kullanılır.
+ * List<int> numaralar = List.filled(10, 0); //10 eleman içeren sabit uzunluklu liste yani dizi.
+ * Index numaraları 0'dan baslar yani listenin ilk elemanına erişmek için 0. indexine bakılır. numaralar[0] diyerek..
+ * 
+ */
+main(List<String> args) {
+  int sayi = 4;
+  List<int> sayilar = List.filled(5, 2, growable: false);
+  sayilar[0] = 4;
+  sayilar[1] = 1;
+  sayilar[2] = 9;
+  print(sayilar);
+  print(sayilar.length);
+  print(sayilar[3]);
+
+}
