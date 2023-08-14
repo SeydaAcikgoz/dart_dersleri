b39ce1a0d37dd9de6a658a0c971974b0f9d391af
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-14 22:10:16 +0300

    Update buyuyen_listeler.dart

--- buyuyen_listeler.dart
+++ buyuyen_listeler.dart
@@ -17,4 +17,14 @@ main(List<String> args) {
   print(sayilar);
   print(sayilar.length);
   sayilar.length = 100;
+
+  List<int> sayilar2 = [1, 2, 3];
+  sayilar2.add(55);
+  print(sayilar2);
+
+  List<int> sayilar3 = List.filled(10, 10, growable: true);
+  sayilar3.add(55);
+  print(sayilar3);
+  print(sayilar3.length);
+
 }
