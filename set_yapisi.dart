cbc441e63d4d8ef9d8e4381ac54c366032e66803
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-15 11:04:16 +0300

    Add files via upload

---
+++ set_yapisi.dart
@@ -0,0 +1,27 @@
+/**
+ * Bir diğer koleksiyon yapısı da Set yapısıdır.
+ * List den en önemli farklı elemanları sıralı olarak tutmaz, bu durumda list lerde oldugu gibi indexleri kullanamayız
+ * Bir diğer farkı ise bir elemandan sadece bir tane olur yani her bir eleman birbirinden farklıdır.
+ * 
+ * index olmadan elemanları kontrol etmek için contains methodu kullanılabilir. onun dışında listelerdeki methodlar set için de geçerlidir.
+ * 
+ */
+
+main(List<String> args) {
+  Set<String> isimler = Set();
+  isimler.add("emre");
+  isimler.add("hasan");
+  isimler.add("ali");
+  isimler.add("emre");
+  isimler.add("emre");
+  isimler.add("fatma");
+  
+  bool sonucum = isimler.remove("emre222");
+  print("sonuc : " + sonucum.toString());
+
+  print("*********");
+
+  for (String s1 in isimler) {
+    print("isim : $s1");
+  }
+}
