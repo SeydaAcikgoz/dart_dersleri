819cdb7515fabce479abca0382afb49f7980e444
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-15 11:04:44 +0300

    Update set_yapisi.dart

--- set_yapisi.dart
+++ set_yapisi.dart
@@ -24,4 +24,33 @@ main(List<String> args) {
   for (String s1 in isimler) {
     print("isim : $s1");
   }
+
+  Set<int> numaralar = Set.from([
+    1,
+    2,
+    3,
+    4,
+    2,
+    1,
+    5,
+    2,
+    1,
+    4,
+    1,
+    1,
+    1,
+    1,
+  ]);
+  List<int> ciftSayilar = [0, 2, 4, 6, 8, 10, 8, 6, 4, 2, 0];
+
+  for (int s1 in numaralar) {
+    print("no : $s1");
+  }
+
+  numaralar.clear();
+  numaralar.addAll(ciftSayilar);
+
+  for (int s1 in numaralar) {
+    print("add alldan sonra no : $s1");
+  }
 }
