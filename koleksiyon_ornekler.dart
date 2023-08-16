4aaf49bb4c76f0fb7456ab649c989f3588886304
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-16 22:08:33 +0300

    Update koleksiyon_ornekler.dart

--- koleksiyon_ornekler.dart
+++ koleksiyon_ornekler.dart
@@ -55,5 +55,31 @@ Sonrasında da bu listeyi okunaklı bir şekilde yazdırın
     print(
         "Listenin ${i + 1}. elemanında bulunan sehir adı: ${oankiSehirMapYapisi['il_adi']} ilce sayısı : ${oankiSehirMapYapisi['ilce_sayisi']} plaka kodu ${oankiSehirMapYapisi['plaka_kodu']} ");
   }
+
+/*4- 5 elemanlı iki farklı liste olusturun. Elemanlar 0-50'ye rastgele şekilde olusturulsun
+Bu elemanları tek bir listeye aktarın
+Olusan son listenin elemanlarının karelerini tutan set yapısı olusturup ekrana yazdırın.
+*/
+  List<int> liste1 = List.filled(5, 0);
+  var liste2 = List<int>.filled(5, 0);
+  List<int> sonListe = <int>[];
+  var sonSetyapisi = <int>{};
+
+  for (int i = 0; i < 5; i++) {
+    liste1[i] = Random().nextInt(50);
+    liste2[i] = Random().nextInt(50);
+  }
+
+  sonListe = [...liste1, ...liste2];
+  
+
+  for (int gecici in sonListe) {
+    sonSetyapisi.add(gecici * gecici);
+  }
+
+  print(sonListe);
+  print(sonSetyapisi);
+
+
 }
 
