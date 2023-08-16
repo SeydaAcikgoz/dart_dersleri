56147991efc6f27b20a9690e6af39cb96d6dee25
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-16 22:08:06 +0300

    Update koleksiyon_ornekler.dart

--- koleksiyon_ornekler.dart
+++ koleksiyon_ornekler.dart
@@ -23,5 +23,37 @@ main(List<String> args) {
   for (var oankiEntry in bilgi.entries) {
     print(" ${oankiEntry.key} : ${oankiEntry.value}");
   }
+
+/*3- Her bir elemanında  keyleri string,  value'leri dynamic  olan bir liste olusturun.
+Bu listedeki her bir eleman il adını, ilçe sayısını, plaka kodunu tutsun.
+Sonrasında da bu listeyi okunaklı bir şekilde yazdırın
+Örnek listenin 1. elemanında bulunan il ankara, plaka kodu:06, ilçe sayısı:10(değerler rastgele olabilir).
+*/
+  List<Map<String, dynamic>> iller = <Map<String, dynamic>>[];
+  Map<String, dynamic> eklenecekSehir1 = Map<String, dynamic>();
+  eklenecekSehir1['il_adi'] = 'ankara';
+  eklenecekSehir1['ilce_sayisi'] = 10;
+  eklenecekSehir1['plaka_kodu'] = 6;
+  Map<String, dynamic> eklenecekSehir2 = <String, dynamic>{};
+  eklenecekSehir2['il_adi'] = 'bursa';
+  eklenecekSehir2['ilce_sayisi'] = 6;
+  eklenecekSehir2['plaka_kodu'] = 16;
+
+  var eklenecekSehir3 = <String, dynamic>{};
+  eklenecekSehir3['il_adi'] = 'istanbul';
+  eklenecekSehir3['ilce_sayisi'] = 16;
+  eklenecekSehir3['plaka_kodu'] = 34;
+
+  iller.add(eklenecekSehir1);
+  iller.add(eklenecekSehir2);
+  iller.add(eklenecekSehir3);
+
+  iller.add({'il_adi': 'izmir', 'ilce_sayisi': 9, 'plaka_kodu': 35});
+
+  for (int i = 0; i < iller.length; i++) {
+    var oankiSehirMapYapisi = iller[i];
+    print(
+        "Listenin ${i + 1}. elemanında bulunan sehir adı: ${oankiSehirMapYapisi['il_adi']} ilce sayısı : ${oankiSehirMapYapisi['ilce_sayisi']} plaka kodu ${oankiSehirMapYapisi['plaka_kodu']} ");
+  }
 }
 
