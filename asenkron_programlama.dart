53494576e6a324c174c78e2e85d600f0b193d694
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-27 12:06:07 +0300

    added short version

--- asenkron_programlama.dart
+++ asenkron_programlama.dart
@@ -22,3 +22,22 @@ Future<String> uzunSurenIslem() {
 
   return sonuc;
 }
+/*Kısa Versiyon
+main(List<String> args) {
+  print("Anne cocugu ekmek almaya yollar");
+  print("Cocuk ekmek almak için evden cıkar");
+  uzunSurenIslem()
+  .then((value) => print(value))
+  .catchError((hata)=>print(hata))
+  .whenComplete(() => print("Ekmek alma operasyonu bitti"));
+
+  print("Peynir zeytin hazırlanır");
+  print("Kahvaltı hazır!");
+}
+
+Future<String> uzunSurenIslem() {
+  return Future<String>.delayed(Duration(seconds: 2), () {
+    return "Çocuk ekmekle eve girer";
+  });
+}
+*/
